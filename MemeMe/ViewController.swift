587ac40e016daa4520a9d16c774a838cb01c8e99
SahilDhawan <sahildhawan2011@gmail.com>
2017-03-11 13:48:47 +0530

    Updated text attributes

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -13,12 +13,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var statusView: UIView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //delegate
         topTextField.delegate = self
         bottomTextField.delegate = self
+        
+        //Specifying the height of status View
+        statusView.translatesAutoresizingMaskIntoConstraints = false
+        let heightContraint = UIApplication.shared.statusBarFrame.height
+        statusView.heightAnchor.constraint(equalToConstant: heightContraint).isActive = true
+        
+        //disabling camera button
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        
+        let memeTextAttributes : [String: Any] = [
+            NSStrokeColorAttributeName : UIColor.black,
+            NSForegroundColorAttributeName : UIColor.white ,
+            NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName : -5.0]
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = .center
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.textAlignment = .center
     }
     
        func showError(msg:String)
@@ -28,6 +50,21 @@ class ViewController: UIViewController {
         activity.addAction(action)
         self.present(activity, animated: true, completion: nil)
     }
+    
+    
+    @IBAction func imagePickerPressed(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        self.present(imagePicker, animated: true, completion: nil)
+    }
 }
 extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate
 {
@@ -35,22 +72,16 @@ extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerD
         self.dismiss(animated: true, completion: nil)
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
+    {
         let key = UIImagePickerControllerOriginalImage
-        showError(msg: "key of the image is   \(key)")
+//        showError(msg: "key of the image is   \(key)")
         if let pickedImage = info[key] as? UIImage
         {
-            showError(msg: "picked Image with key  \(key)")
+//            showError(msg: "picked Image with key  \(key)")
             self.imageView.image = pickedImage
             self.dismiss(animated: true, completion: nil)
         }
-
-        
-    }
-    @IBAction func cameraPressed(_ sender: Any) {
-        let controller = UIImagePickerController()
-        controller.delegate = self
-        self.present(controller, animated: true, completion: nil)
     }
     
 }
