e9a5a672108e39d270a690ad552052d183c623b5
SahilDhawan <sahildhawan2011@gmail.com>
2017-03-16 19:04:01 +0530

    added flowLayout to Collection View

--- MemeMe/MemeCollectionViewController.swift
+++ MemeMe/MemeCollectionViewController.swift
@@ -11,14 +11,8 @@ import UIKit
 class MemeCollectionViewController: UIViewController {
     var memes  = [Meme]()
     @IBOutlet weak var collectionView: UICollectionView!
-    func showAlert(_ msg:String)
-    {
-        let controller = UIAlertController.init(title: "Meme", message:msg , preferredStyle: .alert)
-        let action = UIAlertAction.init(title: "DIsmiss", style: .destructive, handler: nil)
-        controller.addAction(action)
-        present(controller, animated: true, completion: nil)
-    }
-
+    @IBOutlet weak var collectionFlowLayout: UICollectionViewFlowLayout!
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let object = UIApplication.shared.delegate
@@ -26,6 +20,21 @@ class MemeCollectionViewController: UIViewController {
         memes = appDelegate.memes
         collectionView.reloadData()
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        var dimension : CGFloat
+        super.viewWillTransition(to: size, with: coordinator)
+        let space:CGFloat = 10.0
+        dimension = (collectionView.frame.width - space)/2
+        collectionFlowLayout.minimumLineSpacing = space
+        collectionFlowLayout.minimumInteritemSpacing = space
+        if (UIApplication.shared.statusBarOrientation == .landscapeLeft) || (UIApplication.shared.statusBarOrientation == .landscapeRight)
+        {
+            dimension = (collectionView.frame.width-3*space)/4
+        }
+        collectionFlowLayout.itemSize = CGSize(width: dimension, height: dimension)
+    }
+    
     @IBAction func plusButtonPressed(_ sender: Any) {
         let memeController = (storyboard?.instantiateViewController(withIdentifier: "MemeEditor"))! as UIViewController
         self.navigationController?.pushViewController(memeController, animated: true)
@@ -44,14 +53,18 @@ extension MemeCollectionViewController:UICollectionViewDelegate
 //MARK: UICollectionViewDataSouce
 extension MemeCollectionViewController:UICollectionViewDataSource
 {
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return memes.count
     }
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let memeCollectionCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionCell", for: indexPath) as! MemeCollectionViewCell
         let meme = memes[indexPath.row]
         memeCollectionCell.label.text = meme.topText + " " + meme.bottomText
         memeCollectionCell.imageView.image = meme.memedImage
+        memeCollectionCell.layer.borderColor = UIColor.black.cgColor
+        memeCollectionCell.layer.borderWidth = 2.0
         return memeCollectionCell
     }
 }
